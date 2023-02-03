c8ba3318ccf7343a7f43113a069c282c4a0f991b
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-02-03 14:16:20 +0100

    sistemato il search e il display dei risultati

--- resources/js/Pages/Search.vue
+++ resources/js/Pages/Search.vue
@@ -40,10 +40,36 @@
       <h3>Page: {{ pagination.page }} of {{ pagination.pages }}</h3>
     </div>
 
-    <!-- If there are results, I print them here -->
-    <div v-if="results">
-        <h1 v-for="result in results" :key="result.id">{{ result.title }}</h1>
+<!-- If there are results, I print them here -->
+<div v-if="results">
+    <div class="container md:gap-x-4 mx-auto flex flex-wrap justify-around px-5 columns-2 md:columns-4 lg:columns-6">
+
+        <div class="flex flex-col justify-between min-h-72 mb-28 p-3 bg-slate-300 shadow-lg shadow-space-700/80 rounded-xl transition ease-in-out delay-50 duration-300 hover:scale-110 hover:shadow-xl hover:shadow-space-700/90" v-for="record in results" :key="record.id">
+
+            <div class="shadow-md shadow-slate-400 rounded-lg overflow-hidden">
+                <img :src="record.cover_image"
+                class="shrink-0 w-48 object-cover h-full  max-w-full "
+                alt="title">
+            </div>
+
+
+            <div class="w-48 rounded-b-xl h-auto text-center">
+                <h5 class="text-black-500 mb-3">{{ getTitle(record) }}</h5>
+                <h6 class="text-slate-600 mb-4">{{ getAuthor(record) }}</h6>
+            <div class="flex justify-center">
+                <a href=""
+                class="
+                bg-mint-500 p-2 text-bisque-50
+                rounded-xl
+                hover:bg-mint-700">Aggiungi alla libreria</a>
+            </div>
+
+            </div>
+
+        </div>
+
     </div>
+</div>
 
     <!-- Links for paginator -->
 
@@ -64,13 +90,37 @@ let submit = () =>{
     Inertia.post('/query', form)
 }
 
+let getAuthor = (record) => {
 
-defineProps({
+    if(record.title.includes('-')){
+       let words =  record.title.split('-')
+       return words[0].trim();
+    }else {
+        return record.title;
+    }
+
+}
+
+let getTitle = (record) => {
+
+    if(record.title.includes('-')){
+       let words = record.title.split('-')
+       return words[1].trim();
+    }else {
+       return record.title;
+    }
+
+}
+
+
+let props = defineProps({
     results: Object,
     pagination: Object,
     artist: String,
     release_title: String
 });
+
+
 </script>
 
 <script>
@@ -80,6 +130,7 @@ import axios from "axios";
 import { reactive } from "@vue/reactivity";
 import { Link } from "@inertiajs/vue3";
 import Paginator from '@/Components/Paginator.vue';
+import { computed } from '@vue/runtime-core';
 export default {
     layout: Layout,
 };
